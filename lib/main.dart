47fcf3e3fbf95cfa69be0710c37600c0de7dbd6f
Ibrahim Mohammed <hemamohamed.hm35@gmail.com>
2025-01-05 01:28:39 +0200

    theme light and dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quran/manage/dark_and_light_cubit.dart';
+import 'package:quran/routes/routes_settings.dart';
+import 'package:quran/view/presenation/splash_screen.dart';
 
 void main() {
   runApp(Quran());
@@ -8,11 +12,16 @@ class Quran extends StatelessWidget {
   const Quran({super.key, Widgy});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-theme: ThemeData(
-  useMaterial3: true,
+    return  BlocProvider(
+      create: (context) => DarkAndLightCubit(),
+     child:
+        MaterialApp(
+        theme:context.read<DarkAndLightCubit>().isDark==true? ThemeData.light():ThemeData.dark(),
+    onGenerateRoute: RoutesSettings().onGenerateRoute,
+home: Quran_SplashScreen(),
 
-),
+    )
+
     );
   }
 }
